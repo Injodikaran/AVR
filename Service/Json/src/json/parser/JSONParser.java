c0925fa8de4ca47dae714ea1aef56032abab52be
Injodikaran <jinjodikaran@ntb.ch>
2017-03-21 12:02:49 +0100

    Json Parser

---
+++ Service/Json/src/json/parser/JSONParser.java
@@ -0,0 +1,36 @@
+package json.parser;
+import java.io.*;
+import json.*;
+
+public class JSONParser 
+{
+   public static void main(String[] args) 
+   {
+		String jsonData = readFile("properties.json");
+		JSONObject jobj = new JSONObject(jsonData);
+		JSONArray jarr = new JSONArray(jobj.getJSONArray("keywords").toString());
+		System.out.println("Name: " + jobj.getString("name"));
+		for(int i = 0; i < jarr.length(); i++) {
+		    System.out.println("Keyword: " + jarr.getString(i));
+		}
+   }
+
+	public static String readFile(String filename) 
+	{
+	    String result = "";
+	    try {
+	        BufferedReader br = new BufferedReader(new FileReader(filename));
+	        StringBuilder sb = new StringBuilder();
+	        String line = br.readLine();
+	        while (line != null) {
+	            sb.append(line);
+	            line = br.readLine();
+	        }
+	        result = sb.toString();
+	    } catch(Exception e) {
+	        e.printStackTrace();
+	    }
+	    return result;
+	}
+
+}

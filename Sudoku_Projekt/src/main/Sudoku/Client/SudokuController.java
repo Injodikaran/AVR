a18048ca706f107dd6f3cf6942c14705672d04a0
Injodikaran <jinjodikaran@ntb.ch>
2017-04-25 18:41:26 +0200

    Binding Roman

--- Sudoku_Projekt/src/main/Sudoku/Client/SudokuController.java
+++ Sudoku_Projekt/src/main/Sudoku/Client/SudokuController.java
@@ -1,28 +1,32 @@
 package Client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class SudokuController extends Application {
 	private Stage primaryStage;
-    private BorderPane rootLayout;
+
+	@FXML
+	private List<TextField> textFieldList;
 
 	@Override
 	public void start(Stage primaryStage) {
+
 		this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Sudoku");
 
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("SudokuView.fxml"));
-
-			// Timer
-			SudokuTimerTask timerTask = new SudokuTimerTask();
-			timerTask.start();
-			String time = timerTask.getTime();
-
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -31,15 +35,34 @@ public class SudokuController extends Application {
 		}
 	}
 
-	public void newGame(){
-		System.out.println("TEst");
-	}
-
-	public Stage getPrimaryStage() {
-        return primaryStage;
-    }
-
     public static void main(String[] args) {
         launch(args);
     }
+
+	@FXML
+	public void initialize() {
+		/*textField12.textProperty().addListener(new ChangeListener<String>(){
+
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+
+				System.out.println(newValue);
+
+			}textFieldContainer.get(t).textProperty()
+
+		});*/
+
+		for (int i = 0; i < 3; i++) {
+
+	        textFieldList.get(i).textProperty().addListener(new ChangeListener<String>(){
+
+	        	@Override
+				public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+
+					System.out.println(newValue);
+				}
+	        });
+	        i++;
+	    }
+	}
 }
